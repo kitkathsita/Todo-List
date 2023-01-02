50a381b02428bfed835fd9a759533214fcd372bb
kitkath <katherinpgr_99@hotmail.com>
2023-01-02 12:21:08 -0500

    the pop up window for the new task button works now

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css'
+
+const addTaskButton = document.getElementById('newTask')
+const cancelButton = document.getElementById('cancel')
+
+function openForm() {
+  document.getElementById("window").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("window").style.display = "none";
+}
+
+addTaskButton.addEventListener('click', openForm)
+cancelButton.addEventListener('click', closeForm)
